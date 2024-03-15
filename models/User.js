26ff384ae70bad53652c915b028207efad1f782d
Prathamesh Chavan <prathamesh.chavan2000@gmail.com>
2024-03-15 20:29:40 +0530

    USer model and package.json script modified

--- models/User.js
+++ models/User.js
@@ -9,7 +9,6 @@ const UserSchema = new Schema({
   username: {
     type: String,
     required: [true, "Username is required"],
-    unique: [true, "Username already exist"],
     match: [
       /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/,
       "Username invalid, it should contain 8-20 alphanumeric letters and be unique!",
